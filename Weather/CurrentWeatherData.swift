fd3e99d4da175cccb6f2dbf7dc7f50dd869268eb
Aleksandr Reunov <reun_alex@mail.ru>
2020-05-28 01:02:03 +0300

    Parse JSON

---
+++ Weather/CurrentWeatherData.swift
@@ -0,0 +1,29 @@
+//
+//  CurrentWeatherData.swift
+//  Weather
+//
+//  Created by Александр Реунов on 28.05.2020.
+//  Copyright © 2020 Александр Реунов. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+    let feelsLike: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+    }
+}
+
+struct Weather: Codable {
+    let id: Int
+}
