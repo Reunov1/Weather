067bbdf8fddbcd8a704590647debbd7757c37223
Aleksandr Reunov <reun_alex@mail.ru>
2020-05-31 00:35:35 +0300

    application interface update

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -15,24 +15,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
     
-    let networkWeazerManager = NetworkWeazerManager()
-    
+    var networkWeatherManager = NetworkWeatherManager()
     
     @IBAction func searchPressed(_ sender: UIButton) {
-        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { city in
-            self.networkWeazerManager.fetchCurrentWeather(forCity: city) { currentWeather in
-                print(currentWeather.cityName)
-            }
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { [unowned self] city in
+            self.networkWeatherManager.fetchCurrentWeather(forCity: city)
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        networkWeazerManager.fetchCurrentWeather(forCity: "London") { currentWeather in
-            print(currentWeather.cityName)
-
+        networkWeatherManager.onCompletion = { [weak self] currentWeather in
+            guard let self = self else { return }
+            self.updateInterfaceWith(weather: currentWeather)
         }
-        
+        networkWeatherManager.fetchCurrentWeather(forCity: "London")
+    }
+    
+    func updateInterfaceWith(weather: CurrentWeather) {
+        DispatchQueue.main.async {
+            self.cityLabel.text = weather.cityName
+            self.temperatureLabel.text = weather.temperatureString
+            self.feelsLikeTemperatureLabel.text = weather.feelsLikeTemperatureString
+            self.weatherIconImageView.image = UIImage(systemName: weather.systemIconNameString)
+        }
     }
 }
